0e4794764b1f2ab1cc2625d090de7ff90f382aea
Roland Rittchen <rrittchen@gmail.com>
2022-04-10 17:42:38 +0200

    Add comments

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import emoji from 'node-emoji';
 
+// if there is an input in the command line
 if (process.argv.length === 3) {
+  // see if that input coresponds to an emoji and get that emoji
   if (emoji.hasEmoji(process.argv[2])) {
     console.log(emoji.get(process.argv[2]));
+    // else write error message
   } else {
     console.log('Emoji not found');
   }
+  // if no input is provided print a random emoji
 } else {
   console.log(emoji.random()['emoji']);
 }
